5312fb8e3799692a0179f89e6087744aa11d504a
Andreaara <Andreaara>
2025-11-30 20:15:51 -0600

    dependencias duplicadas eliminadas y dos dependencias agregadas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -54,7 +53,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
@@ -82,6 +80,9 @@ dependencies {
     implementation(libs.play.services.location)
 // Mapas (OSM)
     implementation(libs.osmdroid.android)
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
 
 }
