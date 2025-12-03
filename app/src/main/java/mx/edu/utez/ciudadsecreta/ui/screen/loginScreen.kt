6475da6a9fd5a01f13ae98ad2fca64048d603475
Andreaara <Andreaara>
2025-12-02 20:27:11 -0600

    camnbios

--- app/src/main/java/mx/edu/utez/ciudadsecreta/ui/screen/loginScreen.kt
+++ app/src/main/java/mx/edu/utez/ciudadsecreta/ui/screen/loginScreen.kt
@@ -8,22 +8,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.TextAutoSizeDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import mx.edu.utez.ciudadsecreta.viewmodel.LoginViewModel
+import mx.edu.utez.ciudadsecreta.R
 
 
 @Composable
@@ -41,22 +52,37 @@ fun LoginScreen(
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Text("Iniciar Sesión", style = MaterialTheme.typography.headlineLarge)
+        Text("CIUDAD SECRETA", style = MaterialTheme.typography.headlineLarge,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF5E427B)
+        )
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo app",
+            Modifier.size(size = 300.dp)
+        )
+
+        Text("Iniciar Sesión", style = MaterialTheme.typography.headlineLarge,
+            color = Color(0xFF2C1F3B),
+            fontSize = 20.sp
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
 
         OutlinedTextField(
             value = viewModel.nombreUsuario,
             onValueChange = { viewModel.nombreUsuario = it },
             label = { Text("Nombre de Usuario") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.width(380.dp),
+
         )
 
         Spacer(modifier = Modifier.height(8.dp))
@@ -66,7 +92,7 @@ fun LoginScreen(
             onValueChange = { viewModel.contrasena = it },
             label = { Text("Contraseña") },
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.width(380.dp),
         )
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -84,7 +110,7 @@ fun LoginScreen(
         } else {
             Button(
                 onClick = { viewModel.onLoginClick() },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.width(380.dp),
             ) {
                 Text("Entrar")
             }
