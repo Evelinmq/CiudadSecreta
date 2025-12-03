4fe478ca05bc19a8c39a99aa11913f480ced017f
GetsemaniCruz <20243ds149@utez.edu.mx>
2025-12-02 20:49:02 -0600

    quitar

--- app/src/main/java/mx/edu/utez/ciudadsecreta/data/retrofit/ApiService.kt
+++ app/src/main/java/mx/edu/utez/ciudadsecreta/data/retrofit/ApiService.kt
@@ -4,7 +4,6 @@ import mx.edu.utez.ciudadsecreta.data.model.LoginRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import mx.edu.utez.ciudadsecreta.data.model.LoginRequest
 
 interface ApiService {
 
