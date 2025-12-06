4a4e15b19a14dc14ac63acc6a94d2501eab4f866
GetsemaniCruz <20243ds149@utez.edu.mx>
2025-12-05 21:53:20 -0600

    servidor

--- app/src/main/java/mx/edu/utez/ciudadsecreta/data/retrofit/RetrofitClient.kt
+++ app/src/main/java/mx/edu/utez/ciudadsecreta/data/retrofit/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     private const val BASE_URL = "http://187.226.103.73:3000/"
-
+   // "http://192.168.1.76:5000/"
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
